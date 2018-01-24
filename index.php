0ecf034b0020b40100cd56aae6cdbb328b5b478a
dj+401 <dj+401@programmr.com>
2018-01-24 14:00:32 +0000

    FINISH TEST

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if(isset($_GET['delete_id']))
 	$sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
 	mysql_query($sql_query);
 	header("Location: $_SERVER[PHP_SELF]");
-	
+	 
 }
 // delete condition
 
