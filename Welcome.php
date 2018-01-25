f0a5c66d47ab8c70f7fb0ec17eecfccf1880da67
dhananjayharel <harel.dhananjay@gmail.com>
2018-01-25 09:33:11 +0000

    first commit

---
+++ Welcome.php
@@ -0,0 +1,12 @@
+<?php
+function deleteEntry($id){
+	//your code goes here
+	
+}
+
+
+//Solution code(copy into deleteEntry)
+//$sql_query="DELETE FROM users WHERE user_id=".$id;
+//mysql_query($sql_query);
+?>
+
