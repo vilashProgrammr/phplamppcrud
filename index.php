2f76f2b3f02338c9cab98c9a8c366e88a1e794cb
djcheckout <dj@prog1.com>
2018-02-14 11:17:51 +0000

    newcommit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include_once 'dbconfig.php';
 
 // delete condition
 if(isset($_GET['delete_id']))
-{
+{ //djtestnew brnch issue 
 	$sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
 	mysql_query($sql_query);
 	header("Location: $_SERVER[PHP_SELF]");
